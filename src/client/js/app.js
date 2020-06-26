a79fc0803ffa7e954515803e9d4d511c4b625aa8
Trevor Lemon <duhlemman@gmail.com>
2020-06-26 09:11:12 -0700

    Added Create Account form
    - Fleshed out the modal for Create Account.
    - Wired up relationships for going from the Login modal to Create and back again.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,2 +1,12 @@
 const loginForm = document.createElement('custom-login-form');
+const createAccountForm = document.createElement('custom-create-account-form');
+
 loginForm.show();
+loginForm.onCreateClick = () => {
+  loginForm.close();
+  createAccountForm.show();
+  createAccountForm.onCancelClick = () => {
+    createAccountForm.close();
+    loginForm.show();
+  };
+};
