f3a06413a066c2f2cf0f393f659a27fe50625b9d
Nox <the0neWhoKnocks@users.noreply.github.com>
2020-06-27 13:55:09 -0700

    User creation
    - User's can now be added.
    - Username checks occur to prevent overwriting data.
    - After a User is created, their credentials will appear in the Login form.
    - Username and their data are encrypted.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,16 +5,23 @@ if (window.NEEDS_INITAL_SETUP) {
 }
 else {
   const loginForm = document.createElement('custom-login-form');
-  const createAccountForm = document.createElement('custom-create-account-form');
-  createAccountForm.action = '/api/user/create';
-  
-  loginForm.show();
   loginForm.onCreateClick = () => {
     loginForm.close();
     createAccountForm.show();
-    createAccountForm.onCancelClick = () => {
-      createAccountForm.close();
-      loginForm.show();
-    };
   };
+  
+  const createAccountForm = document.createElement('custom-create-account-form');
+  createAccountForm.action = '/api/user/create';
+  createAccountForm.onCreateSuccess = ({ password, username }) => {
+    createAccountForm.close();
+    loginForm.show();
+    loginForm.username = username;
+    loginForm.password = password;
+  };
+  createAccountForm.onCancelClick = () => {
+    createAccountForm.close();
+    loginForm.show();
+  };
+  
+  loginForm.show();
 }
