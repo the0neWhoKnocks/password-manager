255eecfe5eb7f522ff5e6eaca57d84b88e2f9683
Trevor Lemon <duhlemman@gmail.com>
2020-06-29 08:19:56 -0700

    Made `storage` to a `util`

---
+++ src/client/js/storage.js
@@ -0,0 +1,38 @@
+if (!window.utils) window.utils = {};
+
+window.utils.storage = {
+  key: 'pass_man',
+  get: function getStorageData(prop) {
+    let data;
+    let storageType;
+    
+    if (window.sessionStorage[this.key]) storageType = 'sessionStorage';
+    else if (window.localStorage[this.key]) storageType = 'localStorage';
+    
+    if (storageType) {
+      data = JSON.parse(window[storageType].getItem(this.key));
+      if (prop) data = data[prop];
+    }
+    
+    return data;
+  },
+  set: function setStorageData(data, useLocal) {
+    let storageType;
+    
+    // ensure there's only ever one source of truth for data
+    if (useLocal) {
+      storageType = 'localStorage';
+      window.sessionStorage.removeItem(this.key);
+    }
+    else {
+      storageType = 'sessionStorage';
+      window.localStorage.removeItem(this.key);
+    }
+    
+    const currentData = window[storageType].getItem(this.key) || '{}';
+    window[storageType].setItem(this.key, JSON.stringify({
+      ...JSON.parse(currentData),
+      ...data,
+    }));
+  },
+};
