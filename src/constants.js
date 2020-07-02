7e1f419446e46d59856123b7e071903c3463dd55
Nox <the0neWhoKnocks@users.noreply.github.com>
2020-07-02 10:57:25 -0700

    JS files will now auto-load
    When a new JS file is created, the Server will now automatically load it while in `dev`. While in Prod, it'll to one initial lookup on the first load, and pull from the cache until the App is restarted.

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,12 @@
 const { resolve } = require('path');
 
 const ROOT_PATH = resolve(__dirname, './');
+const CLIENT_PATH = `${ROOT_PATH}/client`;
 const DATA_PATH = process.env.DATA_PATH || `${ROOT_PATH}/../data`;
 
-module.exports.CLIENT_PATH = `${ROOT_PATH}/client`;
+module.exports.CLIENT_PATH = CLIENT_PATH;
 module.exports.CONFIG_PATH = `${DATA_PATH}/config.json`;
 module.exports.DATA_PATH = DATA_PATH;
+module.exports.PUBLIC_JS = `${CLIENT_PATH}/js`;
 module.exports.SERVER_PORT = 3000;
 module.exports.USERS_PATH = `${DATA_PATH}/users.json`;
--- src/server/middleware/view.js
+++ src/server/middleware/view.js
@@ -1,13 +1,36 @@
 const { existsSync } = require('fs');
+const glob = require('glob');
 const mkdirp = require('mkdirp');
+const returnErrorResp = require('../utils/returnErrorResp');
 const {
   CONFIG_PATH,
   DATA_PATH,
+  PUBLIC_JS,
 } = require('../../constants');
 
 function ensureFolderStructure() {
   if (!existsSync(DATA_PATH)) mkdirp.sync(DATA_PATH);
 }
+
+let jsFiles = [];
+const getJSFiles = () => new Promise((resolve, reject) => {
+  // Only read the files once 
+  if (!jsFiles.length || process.env.NODE_ENV === 'dev') {
+    glob('/**/*.js', {
+      ignore: [
+        '/**/app.js',
+      ],
+      root: PUBLIC_JS,
+    }, (err, filePaths) => {
+      if (err) reject(err);
+      else {
+        jsFiles = filePaths.map(fp => fp.split(PUBLIC_JS).join('/js'));
+        resolve(jsFiles);
+      }
+    });
+  }
+  else resolve(jsFiles);
+});
 
 const configExists = () => existsSync(CONFIG_PATH);
 
@@ -15,35 +38,33 @@ module.exports = function viewMiddleware({ resp }) {
   ensureFolderStructure();
   
   const NEEDS_INITAL_SETUP = !configExists();
-  const headScripts = [
-    '/js/templates/labeledInput.js',
-    '/js/templates/hrWithText.js',
-    '/js/utils/request.js',
-    '/js/utils/serializeForm.js',
-    '/js/utils/storage.js',
-    '/js/views/configSetUp.js',
-    '/js/views/credentials.js',
-    '/js/views/login.js',
-    '/js/wcs/CustomDialog.js',
-    '/js/wcs/CustomDropDown.js',
-  ];
   
-  resp.end(`
-    <html>
-      <head>
-        <title>Password Manager</title>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=1">
-        
-        <link rel="stylesheet" href="/css/app.css"/>
-        ${headScripts.map(s => `<script src="${s}"></script>`).join(`\n\t\t\t\t`)}
-        <script>
-          window.NEEDS_INITAL_SETUP = ${NEEDS_INITAL_SETUP};
-        </script>
-      </head>
-      <body>
-        <script src="/js/app.js"></script>
-      </body>
-    </html>
-  `);
+  Promise.all([
+    getJSFiles(),
+  ])
+    .then(([
+      jsFilePaths,
+    ]) => {
+      resp.end(`
+        <html>
+          <head>
+            <title>Password Manager</title>
+            <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=1">
+            
+            <link rel="stylesheet" href="/css/app.css"/>
+            ${jsFilePaths.map(s => `<script src="${s}"></script>`).join(`\n\t\t\t`)}
+            <script>
+              window.NEEDS_INITAL_SETUP = ${NEEDS_INITAL_SETUP};
+            </script>
+          </head>
+          <body>
+            <script src="/js/app.js"></script>
+          </body>
+        </html>
+      `);
+    })
+    .catch((err) => {
+      returnErrorResp({ label: 'Failed to render view', resp })(err);
+    });
 }
