4db8af62bc1b84bc70e79f77baef7d2c5b81fdab
Trevor Lemon <duhlemman.dev@gmail.com>
2023-11-15 12:43:13 -0800

    ops: Added Docker development env

---
+++ bin/repo-funcs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CONTAINER="password_manager_dev"
+export REPO_FUNCS=()
+
+# Wire up the current User so that any files created in development can easily
+# be manipulated by the User or during test runs.
+# Export to ensure `docker compose` can use'm
+export CURR_UID=$(id -u)
+export CURR_GID=$(id -g)
+
+REPO_FUNCS+=("startcont")
+function startcont {
+  # ensure required directories are set up
+  mkdir -p ./{.ignore,e2e/mnt/data}
+  
+  if [ ! -d "./certs" ]; then
+    echo -e "\n You need to set up the 'certs' folder.\n If you don't know how, follow the instructions on https://github.com/the0neWhoKnocks/generate-certs."
+    return
+  fi
+
+  # boot container and enter it
+  docker compose build "${CONTAINER}" && docker compose up -d "${CONTAINER}"
+  exitCode=$?
+  if [ $exitCode -ne 0 ]; then
+    echo "[ERROR] Problem starting ${CONTAINER}"
+    return $exitCode
+  fi
+  docker compose exec -u node -it "${CONTAINER}" zsh && docker compose down
+}
+
+REPO_FUNCS+=("entercont")
+function entercont {
+  docker compose exec -u node -it "${CONTAINER}" zsh
+}
