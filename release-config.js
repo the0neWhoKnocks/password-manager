bb2665763f63632f423548f9e8fe6facdcc42f18
Nox <the0neWhoKnocks@users.noreply.github.com>
2023-12-01 14:39:38 -0800

    ops: Decouple release script from repo

---
+++ release-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  version: 1,
+  // URL that the App will be available at once started.
+  APP__TEST_URL: 'https://localhost:3000',
+  // Command to build specific or all Docker containers.
+  CMD__DOCKER_BUILD: 'docker compose build password-manager',
+  // Command to start the built Container(s), so a Dev can verify before it's deployed.
+  CMD__DOCKER_START: 'docker compose up -d password-manager',
+  // Command to compile any assets that may be needed by Docker, shipped off to S3, etc.
+  CMD__COMPILE_ASSETS: 'npm run compile',
+  // Name of the Docker image: <DOCKER_USER>/<NAME>.
+  DOCKER__IMG_NAME: 'theonewhoknocks/password-manager',
+  // When the registry domain is blank, it defaults to `docker.io`.
+  DOCKER__REGISTRY_DOMAIN: '',
+  // Optional when `REPO__HOST` is `github`, otherwise required. Something like `https://<HOSTNAME>/api/v1`.
+  REPO__API_URL: '',
+  // Where your repo is hosted. (Supported: github, gitea)
+  REPO__HOST: 'github',
+};
